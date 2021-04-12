455a59b6d4d999326b2a48fc17b888fd839ef406
IONO <Igor.Ono@analog.com>
2021-04-12 15:43:05 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Read, write and organize directories
+
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+if __name__ == '__main__':
+    f = open('inputFile.txt', 'r')
+    print(f.read())
+    f.close()
+
+
