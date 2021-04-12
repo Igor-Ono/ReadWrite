5387eab474e38fe3cfa64760fa0e976ef9466b89
Igor Ono <iesdrasono@gmail.com>
2021-04-12 16:23:45 +0100

    Read file and print pass lines

--- main.py
+++ main.py
@@ -5,6 +5,14 @@
 #     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 if __name__ == '__main__':
+    # READ file and print only the lines with 'P' on the 3rd element
     f = open('inputFile.txt', 'r')
-    print(f.read())
+    count = 0
+    # Running through every line in the 'inputFile.txt'
+    for line in f:
+        # Split to get the Pass or Fail element (3rd element)
+        line_split = line.split()
+        if line_split[2] == 'P':
+            print(str(count) + ' ' + line)
+        count += 1
     f.close()
