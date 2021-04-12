d3eb01f37730c18c2d2538c2aa16a46236776412
Igor Ono <iesdrasono@gmail.com>
2021-04-12 16:27:12 +0100

    Write pass and fail files

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 # Read, write and organize directories
-
-# def print_hi(name):
-#     # Use a breakpoint in the code line below to debug your script.
-#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Basics of file IO
 
 if __name__ == '__main__':
     # READ file and print only the lines with 'P' on the 3rd element
     f = open('inputFile.txt', 'r')
-    count = 0
+    # WRITE pass file
+    passFile = open('PassFile.txt', 'w')
+    failFile = open('FailFile.txt', 'w')
     # Running through every line in the 'inputFile.txt'
     for line in f:
         # Split to get the Pass or Fail element (3rd element)
         line_split = line.split()
         if line_split[2] == 'P':
-            print(str(count) + ' ' + line)
-        count += 1
+            passFile.write(line)
+        else:
+            failFile.write(line)
+
     f.close()
+    passFile.close()
+    failFile.close()
